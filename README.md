69bf8435f6faef414bdc31fa136327c3c8db174f
queencre <ragnar@crescent.foundation>
2022-09-16 17:15:30 +0900

    feat: working in progress

--- README.md
+++ README.md
@@ -1,19 +1,18 @@
 # Exporter
 
-This repository parses exported genesis state at specific block height to extract data for LUNA and UST holders. 
+This repository parses exported genesis state at specific block height to extract data for Crescent.
 
 ## Background
 
-$UST peg failure leads to the following revival plan - [Terra Ecosystem Revival Plan 2 AMENDED](https://agora.terra.money/t/terra-ecosystem-revival-plan-2-amended/18498). Terraform Labs are planning to fork the currently running chain and distribute new LUNA coins to LUNA & UST holders based on the token distribution outlined in that proposal. Read the revival plan for more context. Some LUNA and UST holders in Crescent Network couldn't transfer their tokens back to Terra chain due to the fact that IBC channel is closed at some point for security reason. That's why this repository is created to provide snapshot data at certain block height to Terraform Labs so that they allocate new LUNA coins for Crescent LUNA and UST holders. 
+...
 
 ## Version
 
-- Crescent [v1.1.0](https://github.com/crescent-network/crescent/releases/tag/v1.1.0)
+- Crescent [v2.1.1](https://github.com/crescent-network/crescent/releases/tag/v2.1.1)
 
 ## Block Heights
 
-- [Pre-attack Block Height 350670](https://www.mintscan.io/crescent/blocks/350670)
-- [Post-attack Block Height 624034](https://www.mintscan.io/crescent/blocks/624034)
+- [Block Height xxx](https://www.mintscan.io/crescent/blocks/xxx)
 
 ## Usage
 
@@ -25,18 +24,9 @@ tar -xvf crescent-exported-xxxxxx.tar.gz
 go run main.go
 ```
 
-
 ## Result File Description
 
-It is important to note that Terra's bip44 coin type 330 can't be derived from just Crescent address. `address_terra` is just conversion of crescent address that uses coin type 118.
-
-| Column | Type | Description | 
-|--------------------|----------|---------------------------------------------------------------------------|
-| address            | string   | The bech32 prefix address for Crescent (CoinType 118)                     | 
-| address_terra      | string   | The bech32 prefix address for Terra (CoinType 118)                        | 
-| luna               | sdk.Coin | The total LUNA Coin for an address                                        | 
-| ust                | sdk.Coin | The total UST Coin for an address                                         |
-| holder             | bool     | Whether or not the address is holding either LUNA or UST in their balance |
-| liquidity_provider | bool     | Whether or not the address is liquidity provider                          |
-| farmer             | bool     | Whether or not the address is farming staker                              |
-
+| Column  | Type   | Description                                           |
+| ------- | ------ | ----------------------------------------------------- |
+| address | string | The bech32 prefix address for Crescent (CoinType 118) |
+| ...     | ...    | ...                                                   |
--- main.go
+++ main.go
@@ -1,21 +1,17 @@
 package main
 
 import (
-	"encoding/csv"
-	"fmt"
 	"log"
-	"os"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/bech32"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
 
-	chain "github.com/crescent-network/crescent/app"
-	chaincmd "github.com/crescent-network/crescent/cmd/crescentd/cmd"
-	farmingtypes "github.com/crescent-network/crescent/x/farming/types"
-	"github.com/crescent-network/crescent/x/liquidity/amm"
-	liquiditytypes "github.com/crescent-network/crescent/x/liquidity/types"
+	chain "github.com/crescent-network/crescent/v2/app"
+	chaincmd "github.com/crescent-network/crescent/v2/cmd/crescentd/cmd"
+	farmingtypes "github.com/crescent-network/crescent/v2/x/farming/types"
+	"github.com/crescent-network/crescent/v2/x/liquidity/amm"
+	liquiditytypes "github.com/crescent-network/crescent/v2/x/liquidity/types"
 )
 
 var (
@@ -28,12 +24,36 @@ var (
 )
 
 const (
-	USTDenom       = "ibc/6F4968A73F90CF7DE6394BF937D6DF7C7D162D74D839C13F53B41157D315E05F"
-	LUNADenom      = "ibc/4627AD2524E3E0523047E35BB76CC90E37D9D57ACF14F0FCBCEB2480705F3CB8"
-	Pool2CoinDenom = "pool2" // bCRE / UST
-	Pool4CoinDenom = "pool4" // LUNA / bCRE
-	Pool5CoinDenom = "pool5" // ATOM / UST
-	Pool6CoinDenom = "pool6" // LUNA / UST
+	CREDenom     = "ucre"
+	bCREDenom    = "ubcre"
+	ATOMDenom    = "ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9"
+	GRAVDenom    = "ibc/C950356239AD2A205DE09FDF066B1F9FF19A7CA7145EA48A5B19B76EE47E52F7"
+	BLDDenom     = "ibc/11F940BCDFD7CFBFD7EDA13F25DA95D308286D441209D780C9863FD4271514EB"
+	USDCgrvDenom = "ibc/CD01034D6749F20AAC5330EF4FD8B8CA7C40F7527AB8C4A302FBD2A070852EE1"
+	USDCaxlDenom = "ibc/BFF0D3805B50D93E2FA5C0B2DDF7E0B30A631076CD80BC12A48C0E95404B4A41"
+	WETHgrvDenom = "ibc/DBF5FA602C46392DE9F4796A0FC7D02F3A8A3D32CA3FAA50B761D4AA6F619E95"
+	WETHaxlDenom = "ibc/F1806958CA98757B91C3FA1573ECECD24F6FA3804F074A6977658914A49E65A3"
+
+	Pool1CoinDenom  = "pool1"  // bCRE / CRE (Basic)
+	Pool22CoinDenom = "pool22" // bCRE / CRE (Ranged)
+	Pool27CoinDenom = "pool27" // bCRE / CRE (Ranged)
+
+	Pool3CoinDenom  = "pool3"  // ATOM / bCRE (Basic)
+	Pool26CoinDenom = "pool26" // ATOM / bCRE (Ranged)
+
+	Pool23CoinDenom = "pool23" // BLD / bCRE (Ranged)
+	Pool24CoinDenom = "pool24" // BLD / bCRE (Ranged)
+
+	Pool10CoinDenom = "pool10" // GRAV / bCRE (Basic)
+	Pool25CoinDenom = "pool25" // GRAV / bCRE (Ranged)
+
+	Pool12CoinDenom = "pool12" // bCRE / USDC.grv (Basic)
+	Pool17CoinDenom = "pool17" // bCRE / USDC.axl (Basic)
+
+	Pool14CoinDenom = "pool14" // WETH.grv / bCRE (Basic)
+	Pool15CoinDenom = "pool15" // WETH.axl / bCRE (Basic)
+
+	// Include Pool2 and Pool4 ? (bCRE/USTC, LUNC/bCRE)
 )
 
 func init() {
@@ -52,11 +72,10 @@ func init() {
 }
 
 type Result struct {
-	LUNA              sdk.Coin // the total LUNA coin for an account
-	UST               sdk.Coin // the total UST coin for an account
-	Holder            bool     // whether or not an account is either LUNA or UST holder
-	LiquidityProvider bool     // whether or not an account is liquidity provider for LUNA or UST pools
-	Farmer            bool     // whether or not an account is farmer for LUNA or UST pools
+	bCRE              sdk.Coin // the total amount of bCRE for an account
+	Holder            bool     // whether or not an account is either CRE or bCRE holder
+	LiquidityProvider bool     // whether or not an account is liquidity provider for CRE or bCRE pools
+	Farmer            bool     // whether or not an account is farmer for CRE or bCRE pools
 }
 
 type PoolMetaData struct {
@@ -106,25 +125,24 @@ func main() {
 	for _, pool := range liquidityGenState.Pools {
 		reserveAddressSet[pool.ReserveAddress] = struct{}{} // pool reserve address
 	}
-	reserveAddressSet[farmingtypes.StakingReserveAcc(Pool2CoinDenom).String()] = struct{}{}
-	reserveAddressSet[farmingtypes.StakingReserveAcc(Pool4CoinDenom).String()] = struct{}{}
-	reserveAddressSet[farmingtypes.StakingReserveAcc(Pool5CoinDenom).String()] = struct{}{}
-	reserveAddressSet[farmingtypes.StakingReserveAcc(Pool6CoinDenom).String()] = struct{}{}
+	reserveAddressSet[farmingtypes.StakingReserveAcc(Pool1CoinDenom).String()] = struct{}{}
+	reserveAddressSet[farmingtypes.StakingReserveAcc(Pool22CoinDenom).String()] = struct{}{}
+	reserveAddressSet[farmingtypes.StakingReserveAcc(Pool27CoinDenom).String()] = struct{}{}
+	// Add more...
 
-	// getLunaUSTAmt is reusable function when parsing LUNA and UST holders, lps, and farmers.
-	getLunaUSTAmt := func(poolCoin sdk.Coin) (lunaAmt, ustAmt sdk.Int) {
-		lunaAmt = sdk.ZeroInt()
-		ustAmt = sdk.ZeroInt()
+	// getbCREAmt is reusable function to return bCRE amount for bCRE holders, lps, and farmers.
+	getbCREAmt := func(poolCoin sdk.Coin) (bCREAmt sdk.Int) {
+		bCREAmt = sdk.ZeroInt()
 
 		pool := poolByPoolCoinDenom[poolCoin.Denom]
 		ammPool := basicPoolByReserveAddress[pool.ReserveAddress]
+
+		amm.Withdraw(rx sdk.Int, ry sdk.Int, ps sdk.Int, pc sdk.Int, feeRate sdk.Dec)
+
 		x, y := ammPool.BasicPool.Withdraw(poolCoin.Amount, sdk.ZeroDec())
 
-		if ammPool.BaseCoin.Denom == LUNADenom {
-			lunaAmt = lunaAmt.Add(y)
-		}
-		if ammPool.QuoteCoin.Denom == USTDenom {
-			ustAmt = ustAmt.Add(x)
+		if ammPool.BaseCoin.Denom == bCREDenom {
+			bCREAmt = bCREAmt.Add(y)
 		}
 		return
 	}
@@ -141,7 +159,7 @@ func main() {
 
 		for _, coin := range balance.GetCoins() {
 			switch coin.Denom {
-			case LUNADenom:
+			case bCREDenom:
 				if _, ok := resultByAddress[balance.Address]; !ok {
 					resultByAddress[balance.Address] = &Result{
 						LUNA: sdk.NewInt64Coin(LUNADenom, 0),
@@ -149,16 +167,6 @@ func main() {
 					}
 				}
 				resultByAddress[balance.Address].LUNA = resultByAddress[balance.Address].LUNA.Add(coin)
-				resultByAddress[balance.Address].Holder = true
-
-			case USTDenom:
-				if _, ok := resultByAddress[balance.Address]; !ok {
-					resultByAddress[balance.Address] = &Result{
-						LUNA: sdk.NewInt64Coin(LUNADenom, 0),
-						UST:  sdk.NewInt64Coin(USTDenom, 0),
-					}
-				}
-				resultByAddress[balance.Address].UST = resultByAddress[balance.Address].UST.Add(coin)
 				resultByAddress[balance.Address].Holder = true
 
 			case Pool2CoinDenom, Pool4CoinDenom, Pool5CoinDenom, Pool6CoinDenom:
@@ -180,158 +188,158 @@ func main() {
 		}
 	}
 
-	// Parsing farmers from staking records
-	for _, record := range farmingGenState.StakingRecords {
-		switch record.StakingCoinDenom {
-		case LUNADenom:
-			resultByAddress[record.Farmer].LUNA = resultByAddress[record.Farmer].LUNA.AddAmount(record.Staking.Amount) // NOT FOUND
-		case USTDenom:
-			resultByAddress[record.Farmer].UST = resultByAddress[record.Farmer].UST.AddAmount(record.Staking.Amount) // NOT FOUND
-		case Pool2CoinDenom, Pool4CoinDenom, Pool5CoinDenom, Pool6CoinDenom:
-			lunaAmt, ustAmt := getLunaUSTAmt(sdk.NewCoin(record.StakingCoinDenom, record.Staking.Amount))
-			if _, ok := resultByAddress[record.Farmer]; !ok {
-				resultByAddress[record.Farmer] = &Result{
-					LUNA: sdk.NewInt64Coin(LUNADenom, 0),
-					UST:  sdk.NewInt64Coin(USTDenom, 0),
-				}
-			}
-			resultByAddress[record.Farmer].UST = resultByAddress[record.Farmer].UST.AddAmount(ustAmt)
-			resultByAddress[record.Farmer].LUNA = resultByAddress[record.Farmer].LUNA.AddAmount(lunaAmt)
-			resultByAddress[record.Farmer].Farmer = true
-		}
-	}
+	// // Parsing farmers from staking records
+	// for _, record := range farmingGenState.StakingRecords {
+	// 	switch record.StakingCoinDenom {
+	// 	case LUNADenom:
+	// 		resultByAddress[record.Farmer].LUNA = resultByAddress[record.Farmer].LUNA.AddAmount(record.Staking.Amount) // NOT FOUND
+	// 	case USTDenom:
+	// 		resultByAddress[record.Farmer].UST = resultByAddress[record.Farmer].UST.AddAmount(record.Staking.Amount) // NOT FOUND
+	// 	case Pool2CoinDenom, Pool4CoinDenom, Pool5CoinDenom, Pool6CoinDenom:
+	// 		lunaAmt, ustAmt := getLunaUSTAmt(sdk.NewCoin(record.StakingCoinDenom, record.Staking.Amount))
+	// 		if _, ok := resultByAddress[record.Farmer]; !ok {
+	// 			resultByAddress[record.Farmer] = &Result{
+	// 				LUNA: sdk.NewInt64Coin(LUNADenom, 0),
+	// 				UST:  sdk.NewInt64Coin(USTDenom, 0),
+	// 			}
+	// 		}
+	// 		resultByAddress[record.Farmer].UST = resultByAddress[record.Farmer].UST.AddAmount(ustAmt)
+	// 		resultByAddress[record.Farmer].LUNA = resultByAddress[record.Farmer].LUNA.AddAmount(lunaAmt)
+	// 		resultByAddress[record.Farmer].Farmer = true
+	// 	}
+	// }
 
-	// Parsing farmers from queued staking records
-	for _, record := range farmingGenState.QueuedStakingRecords {
-		switch record.StakingCoinDenom {
-		case LUNADenom:
-			resultByAddress[record.Farmer].LUNA = resultByAddress[record.Farmer].LUNA.AddAmount(record.QueuedStaking.Amount) // NOT FOUND
-		case USTDenom:
-			resultByAddress[record.Farmer].UST = resultByAddress[record.Farmer].UST.AddAmount(record.QueuedStaking.Amount) // NOT FOUND
-		case Pool2CoinDenom, Pool4CoinDenom, Pool5CoinDenom, Pool6CoinDenom:
-			lunaAmt, ustAmt := getLunaUSTAmt(sdk.NewCoin(record.StakingCoinDenom, record.QueuedStaking.Amount))
-			if _, ok := resultByAddress[record.Farmer]; !ok {
-				resultByAddress[record.Farmer] = &Result{
-					LUNA: sdk.NewInt64Coin(LUNADenom, 0),
-					UST:  sdk.NewInt64Coin(USTDenom, 0),
-				}
-			}
-			resultByAddress[record.Farmer].UST = resultByAddress[record.Farmer].UST.AddAmount(ustAmt)
-			resultByAddress[record.Farmer].LUNA = resultByAddress[record.Farmer].LUNA.AddAmount(lunaAmt)
-			resultByAddress[record.Farmer].Farmer = true
-		}
-	}
+	// // Parsing farmers from queued staking records
+	// for _, record := range farmingGenState.QueuedStakingRecords {
+	// 	switch record.StakingCoinDenom {
+	// 	case LUNADenom:
+	// 		resultByAddress[record.Farmer].LUNA = resultByAddress[record.Farmer].LUNA.AddAmount(record.QueuedStaking.Amount) // NOT FOUND
+	// 	case USTDenom:
+	// 		resultByAddress[record.Farmer].UST = resultByAddress[record.Farmer].UST.AddAmount(record.QueuedStaking.Amount) // NOT FOUND
+	// 	case Pool2CoinDenom, Pool4CoinDenom, Pool5CoinDenom, Pool6CoinDenom:
+	// 		lunaAmt, ustAmt := getLunaUSTAmt(sdk.NewCoin(record.StakingCoinDenom, record.QueuedStaking.Amount))
+	// 		if _, ok := resultByAddress[record.Farmer]; !ok {
+	// 			resultByAddress[record.Farmer] = &Result{
+	// 				LUNA: sdk.NewInt64Coin(LUNADenom, 0),
+	// 				UST:  sdk.NewInt64Coin(USTDenom, 0),
+	// 			}
+	// 		}
+	// 		resultByAddress[record.Farmer].UST = resultByAddress[record.Farmer].UST.AddAmount(ustAmt)
+	// 		resultByAddress[record.Farmer].LUNA = resultByAddress[record.Farmer].LUNA.AddAmount(lunaAmt)
+	// 		resultByAddress[record.Farmer].Farmer = true
+	// 	}
+	// }
 
-	// Dump result file
-	if err := dump(resultByAddress); err != nil {
-		panic(err)
-	}
+	// // Dump result file
+	// if err := dump(resultByAddress); err != nil {
+	// 	panic(err)
+	// }
 
-	// Verify total supply of LUNA and UST are the almost the same as results
-	verify(resultByAddress)
+	// // Verify total supply of LUNA and UST are the almost the same as results
+	// verify(resultByAddress)
 }
 
 func verify(resultByAddress map[string]*Result) {
-	totalLUNAAmt, totalUSTAmt := sdk.ZeroInt(), sdk.ZeroInt()
-	for _, result := range resultByAddress {
-		totalLUNAAmt = totalLUNAAmt.Add(result.LUNA.Amount)
-		totalUSTAmt = totalUSTAmt.Add(result.UST.Amount)
-	}
+	// totalLUNAAmt, totalUSTAmt := sdk.ZeroInt(), sdk.ZeroInt()
+	// for _, result := range resultByAddress {
+	// 	totalLUNAAmt = totalLUNAAmt.Add(result.LUNA.Amount)
+	// 	totalUSTAmt = totalUSTAmt.Add(result.UST.Amount)
+	// }
 
-	log.Println("[Supply]")
-	log.Println("Total LUNA: ", bankGenState.Supply.AmountOf(LUNADenom))
-	log.Println("Total UST: ", bankGenState.Supply.AmountOf(USTDenom))
-	log.Println("")
-	log.Println("[Result]")
-	log.Println("Total LUNA: ", totalLUNAAmt)
-	log.Println("Total UST: ", totalUSTAmt)
-	log.Println("")
-	log.Println("[Truncation]")
-	log.Println("LUNA Diff: ", bankGenState.Supply.AmountOf(LUNADenom).Sub(totalLUNAAmt))
-	log.Println("UST Diff: ", bankGenState.Supply.AmountOf(USTDenom).Sub(totalUSTAmt))
+	// log.Println("[Supply]")
+	// log.Println("Total bCRE: ", bankGenState.Supply.AmountOf(LUNADenom))
+	// log.Println("Total UST: ", bankGenState.Supply.AmountOf(USTDenom))
+	// log.Println("")
+	// log.Println("[Result]")
+	// log.Println("Total LUNA: ", totalLUNAAmt)
+	// log.Println("Total UST: ", totalUSTAmt)
+	// log.Println("")
+	// log.Println("[Truncation]")
+	// log.Println("LUNA Diff: ", bankGenState.Supply.AmountOf(LUNADenom).Sub(totalLUNAAmt))
+	// log.Println("UST Diff: ", bankGenState.Supply.AmountOf(USTDenom).Sub(totalUSTAmt))
 }
 
 func dump(resultByAddress map[string]*Result) error {
-	f, err := os.OpenFile(resultFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
+	// f, err := os.OpenFile(resultFile, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	// if err != nil {
+	// 	return err
+	// }
+	// defer f.Close()
 
-	w := csv.NewWriter(f)
+	// w := csv.NewWriter(f)
 
-	// Write header
-	if err := w.Write([]string{
-		"address",
-		"address_terra",
-		"luna",
-		"ust",
-		"holder",
-		"liquidity_provider",
-		"farmer",
-	}); err != nil {
-		return fmt.Errorf("failed to write: %w", err)
-	}
+	// // Write header
+	// if err := w.Write([]string{
+	// 	"address",
+	// 	"address_terra",
+	// 	"luna",
+	// 	"ust",
+	// 	"holder",
+	// 	"liquidity_provider",
+	// 	"farmer",
+	// }); err != nil {
+	// 	return fmt.Errorf("failed to write: %w", err)
+	// }
 
-	if err := w.Error(); err != nil {
-		return fmt.Errorf("failed to either write or flush: %w", err)
-	}
+	// if err := w.Error(); err != nil {
+	// 	return fmt.Errorf("failed to either write or flush: %w", err)
+	// }
 
-	holderNum := 0
-	lpNum := 0
-	farmerNum := 0
-	totalLUNAAmt := sdk.ZeroInt()
-	totalUSTAmt := sdk.ZeroInt()
-	for addr, result := range resultByAddress {
-		switch {
-		case result.Holder:
-			holderNum++
-		case result.LiquidityProvider:
-			lpNum++
-		case result.Farmer:
-			farmerNum++
-		}
+	// holderNum := 0
+	// lpNum := 0
+	// farmerNum := 0
+	// totalLUNAAmt := sdk.ZeroInt()
+	// totalUSTAmt := sdk.ZeroInt()
+	// for addr, result := range resultByAddress {
+	// 	switch {
+	// 	case result.Holder:
+	// 		holderNum++
+	// 	case result.LiquidityProvider:
+	// 		lpNum++
+	// 	case result.Farmer:
+	// 		farmerNum++
+	// 	}
 
-		if result.LUNA.Denom == LUNADenom {
-			totalLUNAAmt = totalLUNAAmt.Add(result.LUNA.Amount)
-		}
-		if result.UST.Denom == USTDenom {
-			totalUSTAmt = totalUSTAmt.Add(result.UST.Amount)
-		}
+	// 	if result.LUNA.Denom == LUNADenom {
+	// 		totalLUNAAmt = totalLUNAAmt.Add(result.LUNA.Amount)
+	// 	}
+	// 	if result.UST.Denom == USTDenom {
+	// 		totalUSTAmt = totalUSTAmt.Add(result.UST.Amount)
+	// 	}
 
-		// Convert crescent address to terra
-		_, decoded, err := bech32.DecodeAndConvert(addr)
-		if err != nil {
-			return err
-		}
-		terraAddr, err := bech32.ConvertAndEncode("terra", decoded)
-		if err != nil {
-			return err
-		}
+	// 	// Convert crescent address to terra
+	// 	_, decoded, err := bech32.DecodeAndConvert(addr)
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// 	terraAddr, err := bech32.ConvertAndEncode("terra", decoded)
+	// 	if err != nil {
+	// 		return err
+	// 	}
 
-		if err := w.Write([]string{
-			addr,
-			terraAddr,
-			result.LUNA.String(),
-			result.UST.String(),
-			fmt.Sprint(result.Holder),
-			fmt.Sprint(result.LiquidityProvider),
-			fmt.Sprint(result.Farmer),
-		}); err != nil {
-			return err
-		}
-		log.Printf("🏃 Writing content to %s file...\n", f.Name())
-	}
-	w.Flush()
+	// 	if err := w.Write([]string{
+	// 		addr,
+	// 		terraAddr,
+	// 		result.LUNA.String(),
+	// 		result.UST.String(),
+	// 		fmt.Sprint(result.Holder),
+	// 		fmt.Sprint(result.LiquidityProvider),
+	// 		fmt.Sprint(result.Farmer),
+	// 	}); err != nil {
+	// 		return err
+	// 	}
+	// 	log.Printf("🏃 Writing content to %s file...\n", f.Name())
+	// }
+	// w.Flush()
 
-	log.Print("| -----Result------------------------------------------------------")
-	log.Printf("| # of Holders             : %d\n", holderNum)
-	log.Printf("| # of Liquidity Providers : %d\n", lpNum)
-	log.Printf("| # of Farmers             : %d\n", farmerNum)
-	log.Printf("| Total #                  : %d\n", holderNum+lpNum+farmerNum)
-	log.Printf("| Total LUNA Amount        : %s\n", totalLUNAAmt)
-	log.Printf("| Total UST Amount         : %s\n", totalUSTAmt)
-	log.Print("| -----------------------------------------------------------------")
+	// log.Print("| -----Result------------------------------------------------------")
+	// log.Printf("| # of Holders             : %d\n", holderNum)
+	// log.Printf("| # of Liquidity Providers : %d\n", lpNum)
+	// log.Printf("| # of Farmers             : %d\n", farmerNum)
+	// log.Printf("| Total #                  : %d\n", holderNum+lpNum+farmerNum)
+	// log.Printf("| Total LUNA Amount        : %s\n", totalLUNAAmt)
+	// log.Printf("| Total UST Amount         : %s\n", totalUSTAmt)
+	// log.Print("| -----------------------------------------------------------------")
 
 	return nil
 }
